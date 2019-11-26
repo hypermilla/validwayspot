3aa059c47e9a2f209e19836cd376bb7a9b1ed243
hypermilla <hypermilla@protonmail.com>
2019-11-26 00:57:16 -0300

    Results and cover image

--- src/components/results_verdict.js
+++ src/components/results_verdict.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { Component } from "react";
 
 const ResultsVerdict = (props) => {
   return(
-    <div className="alert alert-success" role="alert">
-      <h3 className="verdict">{props.isValid}</h3>
-      <span className="verdict-description">{props.description}</span>
+    <div className={props.alertClass} role="alert">
+      <h3 className="verdict-title">{props.verdictTitle}</h3>
+      <span className="verdict-description">{props.verdictDescription}</span>
     </div>
   );
 }
