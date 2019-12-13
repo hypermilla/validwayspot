75f5828c0c609469f36fd8cc78bc0fc7c2ed104a
hypermilla <hypermilla@protonmail.com>
2019-12-13 00:42:12 -0300

    Is that it?? Footer

---
+++ src/components/homefooter.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+const HomeFooter = (props) => {
+	return (
+		<div className="footer">
+			<p>Wayfarer Index is a free project made to help the Niantic Wayfarer community.</p>
+			<p>Made with ♥ and ReactJS.</p>
+			<div className="row">
+				<div className="col donation">
+					<div className="kofi">
+						<a href='https://ko-fi.com/P5P5196AB' target='_blank'>
+							<img src='https://uploads-ssl.webflow.com/5c14e387dab576fe667689cf/5ca5bf1dff3c03fbf7cc9b3c_Kofi_logo_RGB_rounded-p-500.png' height='24' />
+							Buy me a Ko-fi!
+						</a>
+					</div>
+					<div className="donate-crypto">
+						<h3>Donate some Crypto</h3>
+						<p><b>ETH:</b> <a href="#">milla.eth</a></p>
+						<p><b>BTC:</b> <a href="#">1FiXbrMAQ6RAyCEV43QJzs5HfuZhTyZBbk</a></p>
+					</div>
+				</div>
+				{/* <div className="col links">
+					<h3>Niantic Wayfarer Guides</h3>
+					<ul className="">
+						<li>
+							<a href="" target="_blank">
+								Coming soon!
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div className="col">
+				</div> */}
+			</div>
+		</div>
+	);
+}
+
+export default HomeFooter; 
